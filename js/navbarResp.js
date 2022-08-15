c9491f50d45de0208d48835d6888e593e8c30e5d
Gerardo Schekaiban <ggschekaiban@hotmail.com>
2022-08-15 15:15:21 -0500

    Add header responsiveness

---
+++ js/navbarResp.js
@@ -0,0 +1,29 @@
+const user = document.querySelector('.nav-user');
+const dropdown = document.querySelector('.dropdown');
+
+
+user.addEventListener('click', function(){
+    dropdown.classList.add('display');
+});
+
+const menuIcon = document.querySelector('.menu-icon');
+const navMenu = document.querySelector('.nav-menu');
+const logoWrapp = document.querySelector('.logo-wrapper');
+const auth = document.querySelector('.auth-wrapper');
+const closeIcon = document.querySelector('.close-icon');
+
+menuIcon.addEventListener('click', function(){
+    navMenu.classList.add('open'); 
+    logoWrapp.classList.add('open');
+    auth.classList.add('no-display');
+    menuIcon.classList.add('no-display');
+    closeIcon.classList.add('display');
+});
+
+closeIcon.addEventListener('click', function(){
+    navMenu.classList.remove('open'); 
+    logoWrapp.classList.remove('open'); 
+    menuIcon.classList.remove('no-display');
+    auth.classList.remove('no-display');
+
+});
