080d0c4e94db73d6c951bd0b118f6017fe17a1a8
Gerardo Schekaiban <ggschekaiban@hotmail.com>
2022-08-29 13:06:15 -0500

    Fix header structure

--- assets/js/navbarResp.js
+++ assets/js/navbarResp.js
@@ -1,24 +1,20 @@
-const user = document.querySelector('.nav-user');
+const authWrapper = document.querySelector('.nav-auth');
 const dropdown = document.querySelector('.dropdown');
-const header = document.querySelector('header');
-const body = document.querySelector('body');
 
-user.addEventListener('click', function(){
-    header.classList.toggle('bbottom-none');
+
+authWrapper.addEventListener('click', function(){
     dropdown.classList.toggle('display');
 });
 
-
 const menuIcon = document.querySelector('.menu-icon');
 const navMenu = document.querySelector('.nav-menu');
-const logoWrapp = document.querySelector('.logo-wrapper');
-const auth = document.querySelector('.auth-wrapper');
+const logoWrapp = document.querySelector('.nav-logo');
 const closeIcon = document.querySelector('.close-icon');
 
 menuIcon.addEventListener('click', function(){
     navMenu.classList.add('open'); 
     logoWrapp.classList.add('open');
-    auth.classList.add('no-display');
+    authWrapper.classList.add('no-display');
     menuIcon.classList.add('no-display');
     closeIcon.classList.add('display');
 });
@@ -27,6 +23,5 @@ closeIcon.addEventListener('click', function(){
     navMenu.classList.remove('open'); 
     logoWrapp.classList.remove('open'); 
     menuIcon.classList.remove('no-display');
-    auth.classList.remove('no-display');
-
+    authWrapper.classList.remove('no-display');
 });
